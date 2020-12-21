43f0a6fddb4299cbf2f49a178a6ea9c4f558a81d
Justin Berry <justinberry@Juberr.local>
2020-12-21 18:23:46 -0500

    completed first template of the Mission to Mars website

---
+++ scraping.py
@@ -0,0 +1,86 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+import datetime as dt
+
+executable_path = {'executable_path':'/Users/justinberry/Downloads/chromedriver'}
+browser = Browser('chrome', **executable_path, headless=True)
+
+def mars_news(browser):
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+
+    try: 
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
+
+
+    return news_title, news_p
+
+def featured_image(browser):
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    
+    try:
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+    except AttributeError:
+        return None
+    
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+
+    return img_url
+
+def mars_facts():
+    try:
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+    except BaseException:
+        return None
+
+    df.columns=['Description', 'Mars']
+    df.set_index('Description', inplace=True)
+    return df.to_html()
+
+def scrape_all():
+    exec_path = {'executable_path':'/Users/justinberry/Downloads/chromedriver'}
+    browser = Browser("chrome", **exec_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    
+    browser.quit()
+
+    return data
+
+if __name__ == "__main__":
+    print(scrape_all())
+
